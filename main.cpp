f368b4526024b9c7199cbc2e9e397c2f2c606ac4
Derek <harrison.derek.w@gmail.com>
2021-02-15 15:34:29 +0100

    add comments

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ int main(int argc, char* argv[]) {
 
     /* Parameters */
     N = 2000;       //Number of steps
-    G = 0.2;        //Gravitational constant
-    M = 1.0;        //Mass of gravitating object
+    G = 1.0;        //Gravitational constant
+    M = 0.2;        //Mass of gravitating object
     L = 1.0;        //Angular momentum of orbiting object
 
     u0 = 0.5;       //Initial inverse radius test particle
@@ -35,28 +35,37 @@ int main(int argc, char* argv[]) {
     double x = r * cos(theta);
     double y = r * sin(theta);
 
+    /* Open file for writing data */
     std::ofstream myfile;
     std::string file_name = "data.txt";
     myfile.open(file_name);
 
     /* Evolve orbit */
     for(int i = 0; i < N; ++i) {
+    	/* Advance step */
         u = uh * dtheta + u;
         uh = (3 * G*G * M*M / (L*L) * u*u - u + 1) * dtheta + uh;
+
+        /* Compute radial coordinate */
         r = L*L / (G * M * u);
 
+        /* Compute Cartesian coordinates */
         x = r * cos(theta);
         y = r * sin(theta);
 
+        /* Export location data to file */
         myfile << x << " "
                << y << " "
                << "\n";
 
+        /* Update angle */
         theta = theta + dtheta;
     }
 
+    /* Close file */
     myfile.close();
 
     printf("done\n");
+
     return 0;
 }
